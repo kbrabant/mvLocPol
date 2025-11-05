e88dd10db9f622d4c01ddc2b12bcdbe89e0858b4
kbrabant <kbrabant@iastate.edu>
2025-11-05 08:40:23 -0600

    Update kfun.m

--- src/Octave/kfun.m
+++ src/Octave/kfun.m
@@ -1,1 +1,19 @@
+% Calculates diagonal matrix W = |H|^{-1}K(H^{-1}u) for an arbitrary point xt
+% data with Epanechnikov kernel. X is n x p and xt is 1xp
+% argument of the kernel K(H^{-1}u)
+%
+function W = kfun(arg, kernel, const)
+switch kernel
+    case 'Epa'
+        W = const * max(1 - arg, 0);
+    case 'gauss'
+        W = const * exp(-0.5 * arg);
+    case 'bimodgauss'
+        W = const * arg .* exp(-arg);
+    case 'triangle'
+        W = const * max(1 - sqrt(max(arg, 0)), 0); % Fixed sqrt issue
+    otherwise
+        error('mvlocpol:UnsupportedKernel', 'Unsupported kernel: %s', kernel);
+end
+end
 
