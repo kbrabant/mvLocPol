9b580a4f86bdcc6646d57f3ed9cf8dde8c4d8a58
kbrabant <kbrabant@iastate.edu>
2025-11-06 10:20:34 -0600

    Add files via upload

---
+++ src/Octave/kernel_constants.m
@@ -0,0 +1,17 @@
+%% Calculate the constant to make the kernel a density
+function const = kernel_constants(kernel, d, detH)
+    switch kernel
+        case 'Epa'
+            cd = ((d + 2) * gamma(d/2 + 1)) / (2 * pi^(d/2));
+        case 'gauss'
+            cd = (2 * pi)^(-d / 2);
+        case 'bimodgauss'
+            cd = 2 / (pi^(d/2) * d);
+        case 'triangle'
+            cd = d * (d + 1) * gamma(d / 2) / (2 * pi^(d/2));
+        otherwise
+            error('Unsupported kernel.');
+    end
+    const = cd / detH;
+end
+
