7555666f67da230f1d05415940df00ca00d5b755
kbrabant <kbrabant@iastate.edu>
2025-11-21 19:26:22 -0600

    Update cvlocpol.m

--- src/MATLAB/cvlocpol.m
+++ src/MATLAB/cvlocpol.m
@@ -1,1 +1,11 @@
-
+function [Hopt,loocv,opt] = cvlocpol(X, y, deg, kernel, Hinit)
+%   opt: struct with fields
+%       .H      dxd bandwidth matrix (required)
+%       .k      integer, number of nearest neighbors to use (optional)
+%       .radius scalar, radius for neighbor truncation (optional)
+%       .kernel {'gauss','epa','bimodgauss','triangle'} default 'gauss'
+%       .regularization  scalar added to Gram diag (default 1e-10)
+%       .gpu    logical, default false
+%       .parallel logical, use parallel processing (default false)
+%       .blockSize evaluation points per block (default 2000)
+%       .returnSmoother logical, default false
